22f07cecf9bb608dd340c4c30e21ba4df87f2841
Anam Rani <Anam Rani>
2022-10-15 22:06:36 +0500

    Final commit

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,26 @@ if (age < 50 ) {
     age += 10;
 }
 console.log(age);
+
+// functions
+function calculateTex(income: number,  texYear= 2022) : number{
+    if(texYear < 2022) {
+        return income * 1.2;
+    }
+    return income * 1.3;
+
+}
+calculateTex(10_000);
+
+// objects 
+let employee : {
+    id: number,
+    name: string,
+    retire : (date : Date) => void,
+
+} = {id : 1,
+name: "Anam",
+retire: (date : Date) => {
+    console.log(date);
+}}
+                                    
